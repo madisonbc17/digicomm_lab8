9d1e7a902ac31c9c0b9f7695ab0c6c4358e934e0
Madison Carr <madisonbc17@gmail.com>
2019-10-25 10:12:59 -0600

    I found this online, it works for a parity calculator!

--- parity.py
+++ parity.py
@@ -1,1 +1,29 @@
+#using the brute force section of
+#http://p-nand-q.com/python/algorithms/math/bit-parity.html
+def odd_parity(x):
+    bit = 0
+    num_bits = 0
+    while x:
+        bitmask = 1 << bit
+        bit += 1
+        if x & bitmask:
+            num_bits += 1
+        x &= ~bitmask
+    return num_bits % 1
 
+def even_parity(x):
+    bit = 0
+    num_bits = 0
+    while x:
+        bitmask = 1 << bit
+        bit += 1
+        if x & bitmask:
+            num_bits += 1
+        x &= ~bitmask
+    return num_bits % 2
+
+result_even = even_parity(0x0010)
+result_odd = odd_parity(0x0010)
+
+print(result_even)
+print(result_odd)
